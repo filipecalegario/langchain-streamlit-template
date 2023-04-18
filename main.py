8f3286ce64e34c623cd0876789b709337fdf90be
filipecalegario <fcac@cin.ufpe.br>
2023-04-18 14:47:25 -0300

    adjust avatar initial for user

--- main.py
+++ main.py
@@ -63,4 +63,4 @@ if st.session_state["generated"]:
 
     for i in range(len(st.session_state["generated"]) - 1, -1, -1):
         message(st.session_state["generated"][i], key=str(i), avatar_style="initials", seed="Gonçalves Dias")
-        message(st.session_state["past"][i], is_user=True, key=str(i) + "_user", avatar_style="initials", seed="user")
+        message(st.session_state["past"][i], is_user=True, key=str(i) + "_user", avatar_style="initials", seed="vc")
